f5d95eff6265e1e16fbed1d4578bcd14273779d8
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-12 17:50:08 -0400

    Initial commit. Added Guess method

---
+++ Bonus3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus3
+{
+    class Program
+    {
+        public static int number;
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void CheckGuess(int guess)
+        {
+            if (guess > number)
+            {
+                if (Math.Abs(guess - number) < 10)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                else
+                {
+                    Console.WriteLine("Way too high!");
+                }
+            }
+            else if (guess < number)
+            {
+                if (Math.Abs(guess - number) < 10)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Way too low!");
+                }
+            }
+            else
+            {
+                //Success!!
+            }
+        }
+
+        public static void HowClose(int guess)
+        {
+            if (Math.Abs(guess - number) < 10)
+            {
+
+            }
+        }
+    }
+}
