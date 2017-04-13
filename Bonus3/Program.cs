eaae3f9376a40c8924c8fc33aa3a7736d25aeef0
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-13 09:17:07 -0400

    Added guess loop

--- Bonus3/Program.cs
+++ Bonus3/Program.cs
@@ -8,24 +8,49 @@ namespace Bonus3
 {
     class Program
     {
-        public static int number;
+        public static int number = 50;
 
         static void Main(string[] args)
         {
+            //randomly generate an integer from 1 to 100
+
+            int numberTries = 1;
+            string response;
+            
+            do
+            {
+                if (numberTries > 1)
+                {
+                    response = "again";
+                }
+                else
+                {
+                    response = "the number";
+                }
+
+                numberTries++;
+
+                Console.WriteLine("Guess {0}", response);
+
+
+            } while (CheckGuess(GetNumberInRange(1, 100)));
+            
 
         }
 
-        public static void CheckGuess(int guess)
+        public static bool CheckGuess(int guess)
         {
             if (guess > number)
             {
                 if (Math.Abs(guess - number) < 10)
                 {
                     Console.WriteLine("Too high!");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Way too high!");
+                    return true;
                 }
             }
             else if (guess < number)
@@ -33,15 +58,18 @@ namespace Bonus3
                 if (Math.Abs(guess - number) < 10)
                 {
                     Console.WriteLine("Too low!");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Way too low!");
+                    return true;
                 }
             }
             else
             {
-                //Success!!
+                Console.WriteLine("Great job!");
+                return false;
             }
         }
 
@@ -52,5 +80,28 @@ namespace Bonus3
 
             }
         }
+
+        public static int GetValidInput()
+        {
+            int input;
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Please enter a valid input.");
+            }
+
+            return input;
+        }
+        //Validates that integer is within a given range
+        public static int GetNumberInRange(int min, int max)
+        {
+            int input = GetValidInput();
+            while (input < min || input > max)
+            {
+                Console.WriteLine("Please enter an integer between {0} and {1}.", min, max);
+                input = GetValidInput();
+            }
+            return input;
+        }
+
     }
 }
